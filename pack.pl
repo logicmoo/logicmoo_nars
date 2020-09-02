cfcc601b6a4332cf51cae2e66ae56ba584266bc4
logicmoo <logicmoo@gmail.com>
2020-09-02 09:06:58 -0700

    Narsese: Like OpenNARS in Prolog

---
+++ pack.pl
@@ -0,0 +1,11 @@
+name(narsese).
+version('1.5.666').
+title('Narsese: Like OpenNARS in Prolog').
+keywords([nars,ai,interpeters]).
+author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
+maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
+home( 'https://github.com/TeamSPoon/narsese' ).
+download( 'https://github.com/TeamSPoon/narsese/release/*.zip' ).
+requires(logicmoo_utils).
+autoload(false).
---
+++ prolog/narsese.pl
@@ -0,0 +1,7 @@
+% Non-Axiomatic Logic in Prolog
+% Version: 1.1, September 2012
+% GNU Lesser General Public License
+:- use_module(library(nars/nal)).
+:- use_module(library(nars/nal_reader)).
+
+
