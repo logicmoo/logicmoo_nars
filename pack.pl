c6c2d4d3904cb9a02ee8dc260062b1ce6c6067ef
Douglas R. Miles <logicmoo@gmail.com>
2020-11-11 04:51:52 -0800

    Update pack.pl

--- pack.pl
+++ pack.pl
@@ -5,7 +5,7 @@ keywords([nars,ai,agi,chatbot,interpeters]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
-home( 'https://github.com/TeamSPoon/narsese' ).
-download( 'https://github.com/TeamSPoon/narsese/release/*.zip' ).
+home( 'https://github.com/TeamSPoon/logicmoo_nars' ).
+download( 'https://github.com/TeamSPoon/logicmoo_nars/release/*.zip' ).
 requires(logicmoo_utils).
 autoload(false).
